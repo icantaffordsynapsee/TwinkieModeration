d2ea64189ad96674d5d2f931e30fd994bbe81d1b
icantaffordsynapse <kuleszaleksy@gmail.com>
2026-07-20 21:41:19 +0100

    Modify bot activities and embed colors
    Updated bot status and color configurations.

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,11 +24,21 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "gettin all the e-puh",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
   },
+
+   activities: [
+      {
+        name: "Custom Status", // required by Discord API, not shown in the client
+        state: "the hub",     // this is what people actually see
+        type: 3,               // Custom
+      },
+    ],
+  },
+
 
   // =========================
   // COMMAND BEHAVIOR
@@ -93,8 +103,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#800080",
+      secondary: "#800080",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
